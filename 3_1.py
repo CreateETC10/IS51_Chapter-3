eda789dc0a1d221d99321394d2ccfb1cb9cc48a5
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-03-15 15:51:47 -0700

    class notes

---
+++ 3_1.py
@@ -0,0 +1,16 @@
+""""
+Relational Operator
+<,<=,>,>=,!=,==
+Logical Operator
+and, or, not, in, not in
+"""
+
+# True_or_False= "b" >"a";
+# print("True_or_False", True_or_False)
+
+list1=[6,4,-5,3.5]
+list1.sort()
+print(list1)
+list2=["ha","hi",'B','7']
+list2.sort()
+print(list2)
