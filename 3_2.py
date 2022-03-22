e8a522b3b08c7e9712d3abe3a4e29c5124033b10
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-03-22 15:35:42 -0700

    3_2 changes

---
+++ 3_2.py
@@ -0,0 +1,29 @@
+# grade=90
+# condition= grade>=90
+# if condition:
+#     print("Your grade is A")
+# else:
+#     print("Your grade is not A")
+
+
+# num1=input("Enter the first number:")
+# num2=input("Enter the second number:")
+# print("1", num1, "2", num2)
+
+# # print(isinstance(num1,int))
+
+# if isinstance(num1, str)
+#     print("Data type not allowed! Please specify a numerical value")
+
+# if num1>num2:
+#     largerVal=num1
+# else:
+#     largerVal=num2
+# print("The larger value is"+str(largerVal))
+
+answer=eval(input("How many gallons does a ten-gallon hat hold"))
+if (1.5<=answer<=1):
+    print("Good, ",end="")
+else:
+    print("No,",end="")
+print("It holds about 3/4 of a gallon")
